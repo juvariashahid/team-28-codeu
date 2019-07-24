dc9e5caf39adf3adfaf340d9680557184f0c952f
acamara1498 <acamara@college.harvard.edu>
2019-07-24 09:28:05 -0400

    added the domain to authorized list

--- src/main/webapp/config.js
+++ src/main/webapp/config.js
@@ -1,4 +1,4 @@
 var config = {
-  CLIENT_ID : '60667790168-g95k2mkghuqrgfai28h218h6qd37ue2n.apps.googleusercontent.com',
+  CLIENT_ID : '452803583914-j93u14se1s9nit2gsu49n3dc5fslmf1m.apps.googleusercontent.com',
   API_KEY : 'AIzaSyCUrMnFaWhDb24cCQAKfADYEf4ajWUq_Wg'
 }
